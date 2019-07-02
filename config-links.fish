54cb073cfb76ab91c0d0216c4985852b3021a2c9
Martín Acosta <tinchoz49@gmail.com>
2019-07-02 09:32:12 -0300

    Added tmux-conf.

--- config-links.fish
+++ config-links.fish
@@ -4,8 +4,8 @@ if not test -d ~/.vim
     mkdir ~/.vim
 end
 
-set from "fish/config.fish" "omf" "alacritty/alacritty.yml" ".vimrc" "vim/plug.vim" "vim/plugins" "vim/coc-settings.json" "rofi"
-set to ~/.config/fish/config.fish ~/.config/omf ~/.config/alacritty/alacritty.yml ~/.vimrc ~/.vim/plug.vim ~/.vim/plugins ~/.vim/coc-settings.json ~/.config/rofi
+set from "fish/config.fish" "omf" "alacritty/alacritty.yml" ".vimrc" "vim/plug.vim" "vim/plugins" "vim/coc-settings.json" "rofi" ".tmux.conf"
+set to ~/.config/fish/config.fish ~/.config/omf ~/.config/alacritty/alacritty.yml ~/.vimrc ~/.vim/plug.vim ~/.vim/plugins ~/.vim/coc-settings.json ~/.config/rofi ~/.tmux.conf
 
 for key in $from
   if set -l index (contains -i -- $key $from)
